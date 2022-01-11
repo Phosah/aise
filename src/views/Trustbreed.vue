2036ba6e9998bc3224d5fb31aa5d8643a76aa2d8
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-01-11 06:16:14 -0800

    Update to the trustbreed page

--- src/views/Trustbreed.vue
+++ src/views/Trustbreed.vue
@@ -105,8 +105,8 @@
         <section class="max-w-7xl mx-auto py-20 px-16 bg-brand-black-1 text-white">
             <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">UX competitive analysis</h3>
             <p class="mb-8">I was given two potential competitors by the founders and stakeholders began my study on them and built their marketing profile, SWOT profile, and conducted a UX analysis of their products. The goal was to identify what type of problems users face with competitors’ products, as well as understand what the users might expect from my product.</p>
-            <div class="mb-20"><img src="../assets/ux-competition-analysis-1.png" alt=""></div>
-            <div><img src="../assets/ux-competition-analysis-1.png" alt=""></div>
+            <div class="mb-20"><img class="w-full" src="../assets/ux-competition-analysis-1.png" alt=""></div>
+            <div><img class="w-full" src="../assets/ux-competition-analysis-1.png" alt=""></div>
         </section>
         <section class="max-w-7xl mx-auto py-20 px-16">
             <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">User Stories</h3>
@@ -139,29 +139,193 @@
             <p class="mb-8">Initially, I was running a survey that turned out to be a tremendous information-gathering tool. It helped me to gauge in-depth insights into the target users, including their pains, their frequency, and how they deal with them. Based on the results, I could formulate specific interview questions to find out more about how they deal with poor services and how what they get to do afterward</p>
             <p class="mb-8">Organizing the interviews was the most challenging part of the user research process - but conducting them was highly exciting. Listening to users talk about their experiences with poor services helped me to further refine the problem statement. Below holds some of the questions and the collated results</p>
             <div>
-                <p class="mb-2">1. Have you ever been disappointed with the services provided to you by any company</p>
-                <div><img class="mx-auto" src="../assets/survey-photo.png" alt="Survey photo"></div>
+                <p class="mb-8 font-bold">1. Have you ever been disappointed with the services provided to you by any company</p>
+                <div><img class="w-full mx-auto" src="../assets/survey-photo.png" alt="Survey photo"></div>
                 <div class="flex items-center space-x-12 px-16 md:px-0">
-                        <div class="flex flex-1 space-x-4">
-                            <div class="border-red-400 border-b border-2 w-1/4"></div>
-                            <div class="border-red-400 border-b border-2 w-1/4"></div>
-                            <div class="border-red-400 border-b border-2 w-1/4"></div>
-                            <div class="border-red-400 border-b border-2 w-1/4"></div>
-                        </div>
-                        <div class="flex-1 flex items-center justify-end space-x-4">
-                            <button class="rounded p-3 bg-red-400"><img src="../assets/left-arrow.png" alt="Left arrow"></button>
-                            <button class="rounded-full p-4 bg-red-400"><img src="../assets/right-arrow.png" alt="Right arrow"></button>
-                        </div>
+                    <div class="flex flex-1 space-x-4">
+                        <div class="border-red-400 border-b border-2 w-1/4"></div>
+                        <div class="border-red-400 border-b border-2 w-1/4"></div>
+                        <div class="border-red-400 border-b border-2 w-1/4"></div>
+                        <div class="border-red-400 border-b border-2 w-1/4"></div>
                     </div>
+                    <div class="flex-1 flex items-center justify-end space-x-4">
+                        <button class="rounded p-3 bg-red-400"><img src="../assets/left-arrow.png" alt="Left arrow"></button>
+                        <button class="rounded-full p-4 bg-red-400"><img src="../assets/right-arrow.png" alt="Right arrow"></button>
+                    </div>
+                </div>
             </div>
         </section>
+        <section class="max-w-7xl mx-auto py-20 px-16">
+            <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">User Personas</h3>
+            <p class="mb-8">Based on the generative user research I conducted, I created user personas that capture the essence of my target group. Jasmine and Richard helped me to always keep the needs of the users at the forefront.</p>
+            <div class="flex space-x-10 mb-28">
+                <div class="w-1/3">
+                        <img class="h-full" src="../assets/user-personas-1.png" alt="">
+                        <div class="transform -translate-y-44 pl-6 text-white">
+                            <h3 class="mb-4 text-2xl lg:text-4xl font-semibold font-playfair">Jasmine Kent</h3>
+                            <p class="mb-4">Age: 37 years old</p>
+                            <p class="mb-4">Occupation: Product Manager</p>
+                            <p class="mb-4">Status: Married</p>
+                        </div>
+                </div>
+                <div class="w-2/3">
+                   <h4 class="mb-4 text-xl lg:text-3xl font-bold font-playfair">About</h4>
+                   <p class="mb-8">Richard is a 24yrs old Engineer, he spends a lot of time reading articles and making online purchases for his hardware and engineering materials to be able to work properly</p>
+                   <h4 class="mb-4 text-xl lg:text-3xl font-bold font-playfair">Behaviour & Attitude</h4>
+                   <p class="mb-8">He is calm, intelligent and a bit hyper active at his actions </p>
+                   <h4 class="mb-4 text-xl lg:text-3xl font-bold font-playfair">Goals</h4>
+                   <p class="mb-8">Needs to be able to make his orders purchased and delivered without issues and when damages arise he needs quick resolutions</p>
+                   <h4 class="mb-4 text-xl lg:text-3xl font-bold font-playfair">Concerns</h4>
+                   <p class="mb-8">Occasionally when purchases are made and they are faulty it gets very difficult and frustrating to reach the online store to make changes or refunds</p>
+                </div>
+            </div>
+            <div class="flex space-x-10">
+                <div class="w-1/3">
+                        <img class="h-full" src="../assets/user-personas-2.png" alt="">
+                        <div class="transform -translate-y-44 pl-6 text-white">
+                            <h3 class="mb-4 text-2xl lg:text-4xl font-semibold font-playfair">Richard Lawson</h3>
+                            <p class="mb-4">Age: 24 years old</p>
+                            <p class="mb-4">Occupation: Engineer</p>
+                            <p class="mb-4">Status: Single</p>
+                        </div>
+                </div>
+                <div class="w-2/3">
+                   <h4 class="mb-4 text-xl lg:text-3xl font-bold font-playfair">About</h4>
+                   <p class="mb-8">Jasmine is a 37yrs old project manager who always has to be online due to the remote nature of her job. She works from 9 to 5 from Mondays to Fridays and has a couple of side projects over the weekend</p>
+                   <h4 class="mb-4 text-xl lg:text-3xl font-bold font-playfair">Behaviour & Attitude</h4>
+                   <p class="mb-8">She is calm and collected, patient and like things done quickly as she believes time is precious and should be properly utilized</p>
+                   <h4 class="mb-4 text-xl lg:text-3xl font-bold font-playfair">Goals</h4>
+                   <p class="mb-8">Need to be able to get answers to problems she faces with digital products as she works from home so 90% of services she patronizes are online</p>
+                   <h4 class="mb-4 text-xl lg:text-3xl font-bold font-playfair">Concerns</h4>
+                   <p class="mb-8">Privacy of her messages on services she is having issues with and the time of responses of those issues being as fast as possible</p>
+                </div>
+            </div>
+        </section>
+        <section class="max-w-7xl mx-auto py-20 px-16">
+            <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">Mental models and user journey map</h3>
+            <p class="mb-8">To understand better the challenges surfers are facing in diﬀerent situations, based on the needs and goals of my personas, I created Mental Models and User Journey Maps.</p>
+            <p class="mb-16">Creating Mental Models and User Journey Maps was a great way to empathize with future users, getting a clearer idea about pain points and what support would they expect from the product. </p>
+            <div><img src="../assets/user-map.png" alt="User map"></div>
+        </section>
+        <section class="">
+            <div class="max-w-7xl mx-auto py-20 px-16">
+                <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">User flows</h3>
+                <p class="mb-16">User flows helped me pattern the flow of the users activities in the product from the entry point to the users decisions and the resulting actions to help account for every step of the users journey</p>
+            </div>
+            <div class="max-w-7xl mx-auto"><img src="../assets/user-flows.png" alt="User flows"></div>
+        </section>
+        <section class="">
+            <div class="max-w-7xl mx-auto py-20 px-16">
+                <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">Sitemap</h3>
+                <p class="mb-16">Reviewing my personas and user journeys and having certain features in mind, I started to think about the structure of the app. I had a rough idea of keeping the structure simple with co-existing hierarchies, since there is a great deal of overlapping information and wanted my users to have easy access from one page to the other.</p>
+            </div>
+            <div class="max-w-7xl mx-auto"><img src="../assets/sitemap.png" alt="User flows"></div>
+        </section>
+        <section class="">
+            <div class="max-w-7xl mx-auto py-20 px-16">
+                <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">Wireframing</h3>
+                <p class="mb-16">After that, I started creating wireframes with pen and paper. Having the hand-drawn screens, I made a low-ﬁdelity prototype putting them together to represent the user ﬂow.</p>
+            </div>
+            <div class="max-w-7xl mx-auto"><img src="../assets/wireframe.png" alt="User flows"></div>
+        </section>
+         <section class="max-w-7xl mx-auto py-20 px-16">
+            <div class="mb-16">
+                <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">Onboarding Screens </h3>
+                <p>Tailoring the onboarding to a simple clean visual experience where the users can join the trustbreed using just their name, email and get security authentication through an OTP code. Users are also able to choose or create a customized username.</p>
+                <div class="mb-16"><img class="w-full" src="../assets/onboarding-screen.png" alt="Onboarding screens"></div>
+            </div>
+            <div class="flex items-center justify-between space-x-12 py-20">
+                <div class="flex-1">
+                    <h2 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">Complaints screens</h2>
+                    <p class="">The complaint creation is the centerpiece of the product whereby every user can come into trustbreed and make a complaint about a company that has rendered them poor service. If the company doesn’t exist on trustbreed, you can create them and the company is verified before finally listed. Every complaint submitted would be followed up by trustbreeds agents till the issue is resolved through constant communication with the company and the users</p>
+                </div>
+                <div class="flex-1">
+                    <img class="ml-auto" src="../assets/complaint-screen.png" alt="Complaint screen">
+                </div>
+            </div>
+             <div class="mb-16">
+                <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">Compare Companies</h3>
+                <p>A lot of users are on the lookout for the best product for their needs, due to the long list of competitors for popular services the best is a bit tricky to pick, hence trustbreed gives you the ability to compare companies based on data, and other users ratings and reviews.</p>
+                <div class="mb-16"><img class="w-full" src="../assets/compare-company.png" alt="Compare company"></div>
+             </div>
+        </section>
+         <section class="">
+            <div class="max-w-7xl mx-auto py-20 px-16">
+                <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">Other Features</h3>
+                <p class="mb-16">There are a couple more features like the reviews feature which helps other users know how well you think about a company in case they are surfing for a product to use, the rating feature, and the company activities feature to help be in sync with the upcoming promos or packages companies you follow are bringing out</p>
+            </div>
+            <div class="max-w-7xl mx-auto"><img src="../assets/other-features.png" alt="User flows"></div>
+            <div class="max-w-7xl mx-auto py-20 px-16">
+                <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">Mobile responsiveness</h3>
+                <p class="mb-16">90% of the target users are mobile users and hence to maximize the experience of users, there needs to be a lot of attention paid to the mobile views of the dashboard as that might be the main entry point of most users</p>
+            </div>
+            <div class="max-w-7xl mx-auto"><img src="../assets/mobile-responsive-photo.png" alt="User flows"></div>
+        </section>
+        <section class="max-w-7xl mx-auto py-20 px-16">
+            <div class="mb-16">
+                <h3>Usability Testing</h3>
+                <p class="mb-8">I wanted to observe and measure if users understand the product, its value, and how to complete basic functions through testing the high-ﬁdelity prototype. Below were the test objectives;</p>
+                <ul class="list-disc mb-8 pl-10">
+                    <li>Observe if users can make a complaints easily</li>
+                    <li>Observe if users were able to easily find a particular company they were looking for</li>
+                    <li>Observe if users on making a complaint and didn't find the company if they were able to suggest the company to trustbreed</li>
+                    <li>Observe how easy it was to find companies to compare.</li>
+                </ul>
+                <p>Both in-person and remote moderated usability tests were conducted.  The test included a short brieﬁng and task performance with the Trustbreed. 5 participants took part in the usability test, all participants agreed that the app is uncomplicated and simple to use, including mainly only necessary information and buttons. After collecting the information, I created a rainbow sheet to identify valuable insights.</p>
+            </div>
+            <div class="rounded-lg rounded-t-lg bg-red-300 text-sm">
+                <div class="flex space-x-10 p-4 border-b border-brand-black-4">
+                    <p class="flex-1">Test Feedbacks</p>
+                    <p class="flex-1">Possible Solutions</p>
+                </div>
+                <div class="flex space-x-10 p-4 border-b border-brand-black-4">
+                    <p class="flex-1">Was constantly looking for the search field to easily search for companies and could not find it</p>
+                    <p class="flex-1">The search box should be brought lower to within the section so its easier to spot by users</p>
+                </div>
+                <div class="flex space-x-10 p-4 border-b border-brand-black-4">
+                    <p class="flex-1">It became stressful to select a company category with the horizontal scroll of the categories on the desktop</p>
+                    <p class="flex-1">The categories could be redesigned to show just 4 categories and a dropdown showing more that reveals all the rest categories when cpcked </p>
+                </div>
+                <div class="flex space-x-10 p-4 border-b border-brand-black-4">
+                    <p class="flex-1">On the dashboard, the 4 categories take too much space</p>
+                    <p class="flex-1">The four categories can be merged with one button that has a down showing all the actions that can be taken</p>
+                </div>
+                <div class="flex space-x-10 p-4">
+                    <p class="flex-1">Wanted to make a video of the service I received but could not</p>
+                    <p class="flex-1">Addition of a video upload alongside the photo upload option</p>
+                </div>                   
+            </div>
+        </section>
+        <section class="max-w-7xl mx-auto py-20 px-16">
+            <h3 class="mb-8 text-3xl lg:text-5xl font-bold font-playfair">Conclusion</h3>
+            <p class="mb-16">Throughout the design process, I learned how to prioritize and be very careful in user experience in the product, a product that is built upon the basis of bad user experiences of other products, the user experience could not be compromised. I got the opportunity to. Thank you for following and going through my documented processes. Feel free to connect with me on LinkedIn, Behance, Dribbble, and Twitter.</p>
+        </section>
+         <section class="max-w-7xl mx-auto py-20 px-16">
+            <h2 class="mb-16 text-6xl font-semibold font-playfair text-center">Other Projects</h2>
+            <div class="flex items-center justify-between space-x-8">
+                <router-link to="/"><div><img src="../assets/bitpowr.png" alt="Bitpowr"></div></router-link>
+                <router-link to="/doorstep"><div><img src="../assets/doorstep.png" alt="Doorstep"></div></router-link>
+            </div>
+        </section>
+       <section class="flex flex-col items-center max-w-7xl mx-auto py-32 md:py-20 md:px-16 text-brand-black-1">
+            <div class="mb-24"><img src="../assets/project-talk-dark.png" alt="Project talk-dark"></div>
+            <div class="px-16 md:px-0">
+                <h3 class="mb-8 text-3xl sm:text-4xl md:text-5xl lg:text-7xl text-center md:text-left font-semibold">Let's talk about your project</h3>
+                <p class="mb-16 md:text-xl font-light text-center">It takes any designer to design a product, but it takes creative <span class="font-bold">identification of <br> users needs, and business goals to design the future</span></p>
+            </div>
+            <button class="px-8 py-6 font-bold rounded-md text-white bg-brand-black-1">Book an appointment</button>
+        </section>
+        <div class="">
+            <FooterDark />
+        </div>
   </div>
 </template>
 
 <script>
-import NavbarDark from '@/components/Navbar'
+import NavbarDark from '@/components/NavbarDark.vue'
+import FooterDark from '@/components/FooterDark'
 export default {
-    component: {NavbarDark}
+    components: {NavbarDark, FooterDark}
 }
 </script>
 

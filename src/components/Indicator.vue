baf15be0dd3553cc4788fb8dd4c6c2b93823a0ad
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-02-05 10:02:11 -0800

    Update to Indicator component in carousel

--- src/components/Indicator.vue
+++ src/components/Indicator.vue
@@ -1,21 +1,19 @@
 <template>
-    <div>
-        <ul class="flex space-x-4 items-center">
-            <li
-                v-for="(_, index) in elements"
-                :key="index"
-                class="border-brand-black-2 border-b-4 w-1/4 h-0"
-                :class="{'line-selected': currentElementIndex === index}"
+    <ul class="flex space-x-4 items-center">
+        <li
+            v-for="(_, index) in elements"
+            :key="index"
+            class="border-brand-black-2 border-b-4 w-1/4 h-0"
+            :class="{'line-selected': currentElementIndex === index}"
+        >
+            <button
+                class="absolute border-transparent border-b-4 w-1/4 h-0"
+                @click='showElement(index)'
+                :disabled="currentElementIndex === index"
             >
-                <button
-                    class="bg-red-200 cursor-pointer"
-                    @click='showElement(index)'
-                    :disabled="currentElementIndex === index"
-                >
-                </button>
-            </li>
-        </ul>
-    </div>
+            </button>
+        </li>
+    </ul>
 </template>
 
 <script>
@@ -29,5 +27,8 @@ export default {
 .line-selected {
   opacity: .25;
   cursor: default;
+}
+.line-btn {
+    color: blue;
 }
 </style>
